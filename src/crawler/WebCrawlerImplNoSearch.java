70a9cfb34c2fbe3cf74f98eb4c4fc7d267ca059f
James <jamesghill@yahoo.co.uk>
2015-04-16 22:21:37 +0100

    Initial extension of the 'WebCrawlerImpl' abstract class
    This extension allows for the 'search()' function to be essentially ignored; advanced implementations of this method can be provided in alternative extensions by future programmers.

---
+++ src/crawler/WebCrawlerImplNoSearch.java
@@ -0,0 +1,21 @@
+package crawler;
+
+import java.net.URL;
+
+/**
+ * This extension of the abstract class WebCrawlerImpl provides a 'search()'
+ * method that ignores the parameters and always returns a 'true' value. The
+ * initial version of the WebCrawler application does not search the URL's
+ * gathered by the 'crawl()' method but simply records them all into the results
+ * table. This extension allows the search element to be essentially
+ * ignored.
+ * 
+ * @author James
+ */
+public class WebCrawlerImplNoSearch extends WebCrawlerImpl {
+    
+    @Override
+    public boolean search(URL currentURL, String...searchTerms){
+        return true;
+    }
+}

e5d0758458350db2cd20d99a4be82a3523cf5395
James <jamesghill@yahoo.co.uk>
2015-04-18 18:10:37 +0100

    Simplified the readUntil() method
    I decided that this would only check characters that can appear in the HTML character encoding set.

--- src/crawler/HTMLreadImpl.java
+++ src/crawler/HTMLreadImpl.java
@@ -1,14 +1,12 @@
 package crawler;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 /**
  * This is an implementation of the HTMLread class that is used for parsing
- * web pages.
+ * web pages. This only checks for characters coded in the HTML character set
+ * which is based upon [ISO-8859-1].
  * 
  * @author James Hill
  */
@@ -21,16 +19,14 @@ public class HTMLreadImpl implements HTMLread {
     
     @Override
     public boolean readUntil(InputStream in, char ch1, char ch2){
-        InputStreamReader stream = new InputStreamReader(in, StandardCharsets.UTF_8);
-        BufferedReader buff = new BufferedReader(stream);
         try{
             char check;
-            int next = buff.read();
+            int next = in.read();
             while(next != -1){
                 check = Character.toLowerCase((char)next);
                 if(check == Character.toLowerCase(ch1)){return true;}
                 else if (check == Character.toLowerCase(ch2)){return false;}
-                next = buff.read();
+                next = in.read();
             }
         } catch(IOException exception){
             System.err.println("Error processing stream: " + exception);

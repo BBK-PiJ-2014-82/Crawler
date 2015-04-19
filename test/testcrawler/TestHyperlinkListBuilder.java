34cc0d5e4ce94ddd4104e0a249c787167138e256
James <jamesghill@yahoo.co.uk>
2015-04-19 21:39:38 +0100

    Added checks to ensure the URLs have been constructed correctly.

--- test/testcrawler/TestHyperlinkListBuilder.java
+++ test/testcrawler/TestHyperlinkListBuilder.java
@@ -4,6 +4,7 @@ import crawler.HyperlinkListBuilder;
 import crawler.HyperlinkListBuilderImpl;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -19,37 +20,54 @@ import org.junit.Test;
  */
 public class TestHyperlinkListBuilder {
     
-    // A boolean to be used in the tests;
+    // A boolean to be used in the tests.
     boolean isEmpty = false;
     
     // The HyperlinkListBuilder 
     HyperlinkListBuilder build;
     
+    // A list of InputStream's for testing this class.
+    InputStream doubleStream;
+    InputStream doubleRelativeStream;
+    InputStream mixed2RelativeStream;
+    InputStream mixed4RelativeStream;
+    InputStream mixed4RelativeUpperStream;
+    InputStream singleStream;
+    InputStream singleRelativeStream;
+    InputStream zeroStream;
+    
     // Integer used for holding count of URL's found.
     int listSize;
     
     // List<URL>s for checkingn the results from the build object.
-    List<URL> testList;
+    List<URL> checkList, testList;
     
     // Platform independent line separator.
     String sep = System.getProperty("line.separator");
+    
+    // URL strings.
+    String base = "https://wikileaks.org/index.en.html/";
+    String link1 = "https://wikileaks.org";
+    String link2 = "http://www.google.com";
+    String relative1 = "index";
+    String relative2 = "About";
     
     // Strings for building the mock HTML code.
     String docType = "<!DOCTYPE html>" + sep;
     String closeBody = "</body>" + sep;
     String closeHead = "</head>" + sep;
     String closeHTML = "</html>" + sep;
-    String hyperbase = "<base href=\"https://wikileaks.org/index.en.html/\">" + sep;
+    String hyperbase = "<base href=\""+base+"\">" + sep;
     String hyperbaseUpper = "<BASE href=\"https://wikileaks.org/index.en.html/\">" + sep;
-    String hyperlink1 = "<a href=\"https://wikileaks.org\">Courage is contagious</a>" + sep;
-    String hyperlink2 = "<a href=\"http://www.google.com/\">Search!</a>" + sep;
-    String hyperlinkUpper1 = "<A href=\"https://wikileaks.org\">Courage is contagious</A>" + sep;
-    String hyperlinkUpper2 = "<A href=\"http://www.google.com/\">Search!</A>" + sep;
+    String hyperlink1 = "<a href=\""+link1+"\">Courage is contagious</a>" + sep;
+    String hyperlink2 = "<a href=\""+link2+"\">Search!</a>" + sep;
+    String hyperlinkUpper1 = "<A href=\""+link1+"\">Courage is contagious</A>" + sep;
+    String hyperlinkUpper2 = "<A href=\""+link2+"\">Search!</A>" + sep;
     String openBody = "<body>" + sep;
     String openHead = "<head>" + sep;
     String openHTML = "<html>" + sep;
-    String relativeLink1 = "<a href=\"index\">Index</a>" + sep;
-    String relativeLink2 = "<a href=\"About\">About</a>" + sep;
+    String relativeLink1 = "<a href=\""+relative1+"\">Index</a>" + sep;
+    String relativeLink2 = "<a href=\""+relative2+"\">About</a>" + sep;
     
     // Strings for the URL's that will be created.
     String doubleString;
@@ -61,15 +79,8 @@ public class TestHyperlinkListBuilder {
     String singleRelativeString;
     String zeroString;
     
-    // A list of InputStream's for testing this class.
-    InputStream doubleStream;
-    InputStream doubleRelativeStream;
-    InputStream mixed2RelativeStream;
-    InputStream mixed4RelativeStream;
-    InputStream mixed4RelativeUpperStream;
-    InputStream singleStream;
-    InputStream singleRelativeStream;
-    InputStream zeroStream;
+    // A URL for testing the strings have been parsed correctly.
+    URL testURL1, testURL2, testURL3, testURL4;
     
     @Before
     public void prepare(){
@@ -98,6 +109,12 @@ public class TestHyperlinkListBuilder {
         testList = build.createList(singleStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 1, listSize);
+        try{
+            testURL1 = new URL(link1);
+            assertTrue("URL not in list", testList.contains(testURL1));
+        } catch (MalformedURLException exc) {
+            System.err.println("Error processing stream: " + exc);
+        }
     }
     
     @Test
@@ -114,6 +131,15 @@ public class TestHyperlinkListBuilder {
         testList = build.createList(doubleStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 2, listSize);
+        try{
+            testURL1 = new URL(link1);
+            testURL2 = new URL(link2);
+            checkList.add(testURL1);
+            checkList.add(testURL2);
+            assertTrue("URLs not in list", testList.containsAll(checkList));
+        } catch (MalformedURLException exc) {
+            System.err.println("Error processing stream: " + exc);
+        }
     }
     
     @Test
@@ -132,6 +158,12 @@ public class TestHyperlinkListBuilder {
         testList = build.createList(singleRelativeStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 1, listSize);
+        try{
+            testURL1 = new URL(hyperbase + relativeLink1);
+            assertTrue("URL not in list", testList.contains(testURL1));
+        } catch (MalformedURLException exc) {
+            System.err.println("Error processing stream: " + exc);
+        }
     }
     
     @Test
@@ -151,6 +183,15 @@ public class TestHyperlinkListBuilder {
         testList = build.createList(doubleRelativeStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 2, listSize);
+        try{
+            testURL1 = new URL(hyperbase + relativeLink1);
+            testURL2 = new URL(hyperbase + relativeLink2);
+            checkList.add(testURL1);
+            checkList.add(testURL2);
+            assertTrue("URLs not in list", testList.containsAll(checkList));
+        } catch (MalformedURLException exc) {
+            System.err.println("Error processing stream: " + exc);
+        }
     }
     
     @Test
@@ -170,6 +211,15 @@ public class TestHyperlinkListBuilder {
         testList = build.createList(mixed2RelativeStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 2, listSize);
+        try{
+            testURL1 = new URL(hyperbase + relativeLink1);
+            testURL2 = new URL(hyperlink1);
+            checkList.add(testURL1);
+            checkList.add(testURL2);
+            assertTrue("URLs not in list", testList.containsAll(checkList));
+        } catch (MalformedURLException exc) {
+            System.err.println("Error processing stream: " + exc);
+        }
     }
     
     @Test
@@ -191,6 +241,19 @@ public class TestHyperlinkListBuilder {
         testList = build.createList(mixed4RelativeStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 4, listSize);
+        try{
+            testURL1 = new URL(hyperbase + relativeLink1);
+            testURL2 = new URL(hyperbase + relativeLink2);
+            testURL3 = new URL(hyperlink1);
+            testURL4 = new URL(hyperlink2);
+            checkList.add(testURL1);
+            checkList.add(testURL2);
+            checkList.add(testURL3);
+            checkList.add(testURL4);
+            assertTrue("URLs not in list", testList.containsAll(checkList));
+        } catch (MalformedURLException exc) {
+            System.err.println("Error processing stream: " + exc);
+        }
     }
     
     @Test
@@ -212,5 +275,18 @@ public class TestHyperlinkListBuilder {
         testList = build.createList(mixed4RelativeUpperStream);
         listSize = testList.size();
         assertEquals("Uppercase commands not identified correctly.", 4, listSize);
+        try{
+            testURL1 = new URL(hyperbaseUpper + relativeLink1);
+            testURL2 = new URL(hyperbaseUpper + relativeLink2);
+            testURL3 = new URL(hyperlinkUpper1);
+            testURL4 = new URL(hyperlinkUpper2);
+            checkList.add(testURL1);
+            checkList.add(testURL2);
+            checkList.add(testURL3);
+            checkList.add(testURL4);
+            assertTrue("URLs not in list", testList.containsAll(checkList));
+        } catch (MalformedURLException exc) {
+            System.err.println("Error processing stream: " + exc);
+        }
     }
 }

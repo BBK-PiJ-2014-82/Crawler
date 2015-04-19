4e4ac3092ddfee6ae4df170ef5a6ff35a1992ce7
James <jamesghill@yahoo.co.uk>
2015-04-19 20:07:35 +0100

    Amended the parameter to take an inputStream.

--- src/crawler/HyperlinkListBuilder.java
+++ src/crawler/HyperlinkListBuilder.java
@@ -1,5 +1,6 @@
 package crawler;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
 
@@ -14,14 +15,10 @@ public interface HyperlinkListBuilder {
     
     /**
      * This will create a list of hyperlinks found within the HTML file at the
-     * URL provided and return it as a List object. If the URL does not point
-     * to a HTML file then the method throws an IllegalArgumentException. It is
-     * the responsibility of the programmer using this class to check the URL
-     * before passing it to this method. An IllegalArgumentException is also
-     * thrown if the the connection returns an error.
+     * URL provided and return it as a List object.
      * 
      * @return the list of hyperlinks stored as URLs.
-     * @param webpage this is the HTML document that will be scanned for links.
+     * @param in this is the HTML document streamed for parsing.
      */
-    List<URL> createList(URL webpage) throws IllegalArgumentException;
+    List<URL> createList(InputStream in);
 }

12ca9151a412d1995407d5f8d4f727b186c26d4e
James <jamesghill@yahoo.co.uk>
2015-04-21 22:33:19 +0100

    Added constructors.

--- src/crawler/WebCrawlerImplNoSearch.java
+++ src/crawler/WebCrawlerImplNoSearch.java
@@ -1,6 +1,7 @@
 package crawler;
 
 import java.net.URL;
+import java.sql.Connection;
 
 /**
  * This extension of the abstract class WebCrawlerImpl provides a 'search()'
@@ -13,6 +14,30 @@ import java.net.URL;
  * @author James Hill
  */
 public class WebCrawlerImplNoSearch extends WebCrawlerImpl {
+    
+    /**
+     * The basic constructor for this class.
+     * 
+     * @param URL the initial URL that the webcrawler will start from.
+     * @param conn the database connection the webcrawler will write/read to.
+     */
+    public WebCrawlerImplNoSearch(String URL, Connection conn) {
+        super(URL, conn);
+    }
+    
+    /**
+     * This is the secondary constructor for this class that can also take in
+     * arguments for the maximum number of links to be searched or the
+     * maximum depth of the pages that will be searched.
+     * 
+     * @param URL the initial URL that the webcrawler will start from.
+     * @param conn the database connection the webcrawler will write/read to.
+     * @param maxLinks the maximum number of links to be processed.
+     * @param maxDepth the maximum depth of web pages to be processed.
+     */
+    public WebCrawlerImplNoSearch(String URL, Connection conn, Integer maxLinks, Integer maxDepth) {
+        super(URL, conn, maxLinks, maxDepth);
+    }
     
     @Override
     public boolean search(URL currentURL, String...searchTerms){

c6e878208cffffdde80d7de8963ea286bd14b835
James <jamesghill@yahoo.co.uk>
2015-04-21 18:31:54 +0100

    Added new test to check for spaces around the html commands.

--- test/testcrawler/TestHyperlinkListBuilder.java
+++ test/testcrawler/TestHyperlinkListBuilder.java
@@ -33,6 +33,7 @@ public class TestHyperlinkListBuilder {
     InputStream doubleRelativeStream;
     InputStream mixed2RelativeStream;
     InputStream mixed4RelativeStream;
+    InputStream mixed4RelativeSpacedStream;
     InputStream mixed4RelativeUpperStream;
     InputStream singleStream;
     InputStream singleRelativeStream;
@@ -60,22 +61,29 @@ public class TestHyperlinkListBuilder {
     String closeHead = "</head>" + sep;
     String closeHTML = "</html>" + sep;
     String hyperbase = "<base href=\""+base+"\">" + sep;
+    String hyperbaseSpaced = "<  base href=\""+base+"\">" + sep;
     String hyperbaseUpper = "<BASE href=\""+base+"\">" + sep;
     String hyperlink1 = "<a href=\""+link1+"\">Courage is contagious</a>" + sep;
+    String hyperlink1Spaced = "<  a  href=\""+link1+"\">Courage is contagious</a>" + sep;
     String hyperlink2 = "<a href=\""+link2+"\">Search!</a>" + sep;
+    String hyperlink2Spaced = "< a href=\""+link2+"\">Search!</a>" + sep;
     String hyperlinkUpper1 = "<A href=\""+link1+"\">Courage is contagious</A>" + sep;
     String hyperlinkUpper2 = "<A href=\""+link2+"\">Search!</A>" + sep;
     String openBody = "<body>" + sep;
+    String openBodySpaced = "< body >" + sep;
     String openHead = "<head>" + sep;
     String openHTML = "<html>" + sep;
     String relativeLink1 = "<a href=\""+relative1+"\">Index</a>" + sep;
+    String relativeLink1Spaced = "< a  href=\""+relative1+"\">Index</a>" + sep;
     String relativeLink2 = "<a href=\""+relative2+"\">About</a>" + sep;
+    String relativeLink2Spaced = "<  a  href=\""+relative2+"\">About</a>" + sep;
     
     // Strings for the URL's that will be created.
     String doubleString;
     String doubleRelativeString;
     String mixed2RelativeString;
     String mixed4RelativeString;
+    String mixed4RelativeSpacedString;
     String mixed4RelativeUpperString;
     String singleString;
     String singleRelativeString;
@@ -92,7 +100,8 @@ public class TestHyperlinkListBuilder {
     @Test
     public void checkCreateListReturnsEmptyListIfZeroHyperlinks(){
         zeroString = "";
-        zeroStream = new ByteArrayInputStream(zeroString.getBytes(StandardCharsets.ISO_8859_1));
+        zeroStream = new ByteArrayInputStream(
+                zeroString.getBytes(StandardCharsets.ISO_8859_1));
         testList = build.createList(zeroStream);
         isEmpty = testList.isEmpty();
         assertTrue("The returned List is not empty.", isEmpty);
@@ -107,7 +116,8 @@ public class TestHyperlinkListBuilder {
                 hyperlink1 +
                 closeBody +
                 closeHTML;
-        singleStream = new ByteArrayInputStream(singleString.getBytes(StandardCharsets.ISO_8859_1));
+        singleStream = new ByteArrayInputStream(
+                singleString.getBytes(StandardCharsets.ISO_8859_1));
         testList = build.createList(singleStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 1, listSize);
@@ -132,7 +142,8 @@ public class TestHyperlinkListBuilder {
                 hyperlink2 +
                 closeBody +
                 closeHTML;
-        doubleStream = new ByteArrayInputStream(doubleString.getBytes(StandardCharsets.ISO_8859_1));
+        doubleStream = new ByteArrayInputStream(
+                doubleString.getBytes(StandardCharsets.ISO_8859_1));
         testList = build.createList(doubleStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 2, listSize);
@@ -163,7 +174,8 @@ public class TestHyperlinkListBuilder {
                 relativeLink1 +
                 closeBody +
                 closeHTML;
-        singleRelativeStream = new ByteArrayInputStream(singleRelativeString.getBytes(StandardCharsets.ISO_8859_1));
+        singleRelativeStream = new ByteArrayInputStream(
+                singleRelativeString.getBytes(StandardCharsets.ISO_8859_1));
         testList = build.createList(singleRelativeStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 1, listSize);
@@ -191,7 +203,8 @@ public class TestHyperlinkListBuilder {
                 relativeLink2 +
                 closeBody +
                 closeHTML;
-        doubleRelativeStream = new ByteArrayInputStream(doubleRelativeString.getBytes(StandardCharsets.ISO_8859_1));
+        doubleRelativeStream = new ByteArrayInputStream(
+                doubleRelativeString.getBytes(StandardCharsets.ISO_8859_1));
         testList = build.createList(doubleRelativeStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 2, listSize);
@@ -223,7 +236,8 @@ public class TestHyperlinkListBuilder {
                 hyperlink1 +
                 closeBody +
                 closeHTML;
-        mixed2RelativeStream = new ByteArrayInputStream(mixed2RelativeString.getBytes(StandardCharsets.ISO_8859_1));
+        mixed2RelativeStream = new ByteArrayInputStream(
+                mixed2RelativeString.getBytes(StandardCharsets.ISO_8859_1));
         testList = build.createList(mixed2RelativeStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 2, listSize);
@@ -257,7 +271,8 @@ public class TestHyperlinkListBuilder {
                 hyperlink2 +
                 closeBody +
                 closeHTML;
-        mixed4RelativeStream = new ByteArrayInputStream(mixed4RelativeString.getBytes(StandardCharsets.ISO_8859_1));
+        mixed4RelativeStream = new ByteArrayInputStream(
+                mixed4RelativeString.getBytes(StandardCharsets.ISO_8859_1));
         testList = build.createList(mixed4RelativeStream);
         listSize = testList.size();
         assertEquals("The List size is incorrect.", 4, listSize);
@@ -295,7 +310,8 @@ public class TestHyperlinkListBuilder {
                 hyperlinkUpper2 +
                 closeBody +
                 closeHTML;
-        mixed4RelativeUpperStream = new ByteArrayInputStream(mixed4RelativeUpperString.getBytes(StandardCharsets.ISO_8859_1));
+        mixed4RelativeUpperStream = new ByteArrayInputStream(
+                mixed4RelativeUpperString.getBytes(StandardCharsets.ISO_8859_1));
         testList = build.createList(mixed4RelativeUpperStream);
         listSize = testList.size();
         assertEquals("Uppercase commands not identified correctly.", 4, listSize);
@@ -311,6 +327,45 @@ public class TestHyperlinkListBuilder {
             checkList.add(testURL4);
             assertTrue("URLs not in list", testList.containsAll(checkList));
             mixed4RelativeUpperStream.close();
+        } catch (MalformedURLException exc) {
+            System.err.println("Error processing stream: " + exc);
+        } catch (IOException exc) {
+            System.err.println("Error processing stream: " + exc);
+        }
+    }
+    
+    @Test
+    public void checkCreateListReturnsMixedFourHyperlinksWithExtraSpaces(){
+        mixed4RelativeSpacedString =
+                docType +
+                openHead +
+                hyperbaseSpaced +
+                closeHead +
+                openHTML +
+                openBodySpaced +
+                relativeLink1Spaced +
+                hyperlink1Spaced +
+                relativeLink2Spaced +
+                hyperlink2Spaced +
+                closeBody +
+                closeHTML;
+        mixed4RelativeSpacedStream = new ByteArrayInputStream(
+                mixed4RelativeSpacedString.getBytes(StandardCharsets.ISO_8859_1));
+        testList = build.createList(mixed4RelativeSpacedStream);
+        listSize = testList.size();
+        assertEquals("The List size is incorrect.", 4, listSize);
+        try{
+            testURL1 = new URL(hyperbase + relativeLink1);
+            testURL2 = new URL(hyperbase + relativeLink2);
+            testURL3 = new URL(hyperlink1);
+            testURL4 = new URL(hyperlink2);
+            checkList = new LinkedList<>();
+            checkList.add(testURL1);
+            checkList.add(testURL2);
+            checkList.add(testURL3);
+            checkList.add(testURL4);
+            assertTrue("URLs not in list", testList.containsAll(checkList));
+            mixed4RelativeSpacedStream.close();
         } catch (MalformedURLException exc) {
             System.err.println("Error processing stream: " + exc);
         } catch (IOException exc) {

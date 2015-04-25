9a82a9385ab1df2ba9efcc8f14a118272d8f8b6a
James <jamesghill@yahoo.co.uk>
2015-04-25 15:34:34 +0100

    Changed parameters and return type of the crawler method.

--- src/crawler/WebCrawler.java
+++ src/crawler/WebCrawler.java
@@ -1,14 +1,15 @@
 package crawler;
 
-import java.io.File;
 import java.net.URL;
+import java.sql.Connection;
+import java.util.LinkedList;
 
 /**
  * The web crawler is a class that constructs a database of URL links.  Having
  * been provided a link it will perform a 'breadth-first' search of the links
- * that it can follow from the 1st web page using a temporary file to store
+ * that it can follow from the 1st web page using a database to store
  * a record of it's progress. It then stores results that meet criteria defined
- * in implementations of the interface in a results file.
+ * in implementations of the interface in a LinkedList that is returned.
  * 
  * @author James Hill
  */
@@ -18,14 +19,13 @@ public interface WebCrawler {
      * This crawl method opens a HTTP connection to the starting URL and reviews
      * the entire web page, saving links into a temporary table to record the
      * progress of the crawler through it's crawl. A results table is also
-     * provided so that URL's which meet a criteria to be specified by the
+     * created so that URL's which meet a criteria to be specified by the
      * programmer in implementations of the interface can be stored for later
      * review by the program user.
      * 
-     * @return the results file constructed by the crawler.
+     * @return the a LinkedList of all links found by the WebCrawler.
      * @param startURL the URL from which the WebCrawler will start crawling.
-     * @param temp the file holding temporary links for crawling.
-     * @param results the file holding links that meet specified criteria.
+     * @param conn the connection that will be used to build the database.
      */
-    File crawl(URL startURL, File temp, File results);
+    LinkedList<String> crawl(URL startURL, Connection conn);
 }

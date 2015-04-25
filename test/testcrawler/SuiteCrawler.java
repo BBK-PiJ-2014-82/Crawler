b486c16d6e870501fe4b89ec9341ed893b130cf3
James <jamesghill@yahoo.co.uk>
2015-04-25 15:21:55 +0100

    Added missing JavaDoc and added in new TestlinkDB class

--- test/testcrawler/SuiteCrawler.java
+++ test/testcrawler/SuiteCrawler.java
@@ -3,11 +3,18 @@ package testcrawler;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * This is a testing suite that runs all previously created test classes for
+ * this project.
+ * 
+ * @author James Hill
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
         {
             TestHTMLread.class,
             TestHyperlinkListBuilder.class,
+            TestLinkDB.class
         })
 
 public class SuiteCrawler {}

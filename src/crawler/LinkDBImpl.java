a258b102a30c2c11cf2188943276e57d531d8535
James <jamesghill@yahoo.co.uk>
2015-04-26 19:51:14 +0100

    Added new method getPriority.

--- src/crawler/LinkDBImpl.java
+++ src/crawler/LinkDBImpl.java
@@ -102,6 +102,24 @@ public class LinkDBImpl implements LinkDB {
     }
     
     @Override
+    public int getPriority(String link){
+        int priority = -1;
+        try {
+            ResultSet result;
+            state = conn.createStatement();
+            result = state.executeQuery("SELECT Priority "
+                    + "FROM Temp "
+                    + "WHERE Link = '" + link + "'");
+            if(result.next()){
+                priority = result.getInt(1);
+            }
+        } catch (SQLException exc) {
+            System.err.println("Error processing stream: " + exc);
+        }
+        return priority;
+    }
+    
+    @Override
     public void linkVisited(String link){
         try {
             state = conn.createStatement();
